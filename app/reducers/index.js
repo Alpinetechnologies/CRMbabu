d93feeceb82f94f3bc00a06cf9f4026b5c6f7167
Yash Gupta <yg123yash@gmail.com>
2020-09-24 18:51:28 +0530

    Update index.js

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,17 +1,1 @@
-/*
- * combines all th existing reducers
- */
-import * as loadingReducer from './loadingReducer';
-import * as loginReducer from './loginReducer';
-import * as themeReducer from './themeReducer';
-import * as leadsReducer from './leadsReducer';
-import * as opportunityReducer from './opportunityReducer';
-import * as dashboardReducer from './dashboardReducer';
-export default Object.assign(
-  loginReducer,
-  loadingReducer,
-  themeReducer,
-  leadsReducer,
-  opportunityReducer,
-  dashboardReducer,
-);
+
